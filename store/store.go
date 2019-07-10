964a82aeb72c69fab036cd7cc6b25e5e7cabe376
Alexander Daskalov <rjuan33@gmail.com>
2019-07-10 17:28:05 +0300

    restructured store and added interface + basic impl + basic testing

--- store/store.go
+++ store/store.go
@@ -1,6 +1,9 @@
 package store
 
-import "math/rand"
+import (
+	"errors"
+	"math/rand"
+)
 
 type Vector struct {
 	id     string
@@ -33,4 +36,39 @@ func (v *Vector) Sum() float64 {
 		sum += element
 	}
 	return sum
+}
+
+type Store interface {
+	Get(id string) (*Vector, error)
+	Set(id string, vector *Vector) error
+	Delete(id string) error
+	KNN(vector *Vector, k int) ([]*Vector, error)
+}
+
+type SimpleMapStore map[string]*Vector
+
+func newSimpleMapStore() SimpleMapStore {
+	return make(map[string]*Vector)
+}
+
+func (s *SimpleMapStore) Get(id string) (*Vector, error) {
+	var value, found = (*s)[id]
+	if found {
+		return value, nil
+	}
+	return value, errors.New("id not found")
+}
+
+func (s *SimpleMapStore) Set(id string, vector *Vector) error {
+	(*s)[id] = vector
+	return nil
+}
+
+func (s *SimpleMapStore) Delete(id string) error {
+	delete(*s, id)
+	return nil
+}
+
+func (s *SimpleMapStore) KNN(vector *Vector, k int) ([]*Vector, error) {
+	return nil, nil
 }
--- store/store_test.go
+++ store/store_test.go
@@ -8,3 +8,40 @@ func TestSum(t *testing.T) {
 		t.Errorf("Wrong sum")
 	}
 }
+
+func set(s Store, id string, vector *Vector) error {
+	return s.Set(id, vector)
+}
+
+func get(s Store, id string) (*Vector, error) {
+	return s.Get(id)
+}
+
+func del(s Store, id string) error {
+	return s.Delete(id)
+}
+func testStore(s Store, t *testing.T) {
+	var ones = Ones("ones", 32)
+	var zeros = Zeros("zeros", 32)
+	var randoms = Random("rand", 32)
+
+	set(s, ones.id, ones)
+	set(s, zeros.id, zeros)
+	set(s, randoms.id, randoms)
+
+	var val, err = get(s, ones.id)
+	if err != nil || val != ones {
+		t.Error("error getting values")
+	}
+
+	del(s, ones.id)
+	val, err = get(s, ones.id)
+	if err == nil {
+		t.Error("can get value that should have been deleted")
+	}
+}
+
+func TestSimpleMapStore(t *testing.T) {
+	var s = newSimpleMapStore()
+	testStore(&s, t)
+}
