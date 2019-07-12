465b9590410d5fbb3bc5c3a6e9a947243a12ec73
Alexander Daskalov <rjuan33@gmail.com>
2019-07-12 17:37:25 +0300

    vector unmarshalling

--- server/controllers.go
+++ server/controllers.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/Comonut/vectorugo/store"
 )
 
 func Init() {
@@ -21,7 +25,21 @@ func handleVectors(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		fmt.Fprint(w, "GET")
 	case "POST":
-		fmt.Fprint(w, "POST")
+
+		var v store.Vector
+		b, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(b, &v)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Invalid json format:	", err)
+			return
+		}
+		fmt.Fprint(w, v.Sum())
+
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprint(w, "ERROR")
