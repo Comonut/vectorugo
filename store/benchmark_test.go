9f4310ef3e37d5d068901160b5145ebee9b41913
Alexander Daskalov <rjuan33@gmail.com>
2019-09-18 14:34:24 +0300

    Search benchmark (#16)
    * added knn benchmark
    * fixed search benchmark

--- store/benchmark_test.go
+++ store/benchmark_test.go
@@ -9,15 +9,16 @@ import (
 
 // STORAGE BENCHMARKS
 // YOU CAN RUN WITH 'go test ./store -bench=.'
-// TODO: SEARCH BENCHMARKS
 
 //FOR PREVENTING COMPILER OPTIMIZATIONS
 var e error
 var f Vector
+var d *[]Distance
 
 func BenchmarkInsertMemoryStore(b *testing.B) {
 	store := NewSimpleMapStore()
 	var err error
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		err = store.Set(strconv.Itoa(n), Random(strconv.Itoa(n), 256))
 	}
@@ -30,6 +31,7 @@ func BenchmarkGetMemoryStore(b *testing.B) {
 		_ = store.Set(strconv.Itoa(n), Random(strconv.Itoa(n), 256))
 	}
 	var err error
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		f, err = store.Get(strconv.Itoa(rand.Intn(100)))
 	}
@@ -39,6 +41,7 @@ func BenchmarkGetMemoryStore(b *testing.B) {
 func BenchmarkInsertPersistantStore(b *testing.B) {
 	store, _ := NewPersitantStore(256, "benchmark_index.bin", "benchmark_vectors.bin", "benchmark_search.bin")
 	var err error
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		err = store.Set(strconv.Itoa(n), Random(strconv.Itoa(n), 256))
 	}
@@ -55,6 +58,7 @@ func BenchmarkGetPersistantStore(b *testing.B) {
 	}
 	var err error
 	var v Vector
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		v, err = store.Get(strconv.Itoa(rand.Intn(100)))
 	}
@@ -62,5 +66,23 @@ func BenchmarkGetPersistantStore(b *testing.B) {
 	f = v
 	os.Remove("benchmark_index.bin")
 	os.Remove("benchmark_vectors.bin")
-	os.Remove("benchmakr_search.bin")
+	os.Remove("benchmark_search.bin")
+}
+
+func BenchmarkKNNsearch(b *testing.B) {
+	store, _ := NewPersitantStore(256, "benchmark_index.bin", "benchmark_vectors.bin", "benchmark_search.bin")
+	for n := 0; n < 100; n++ {
+		_ = store.Set(strconv.Itoa(n), Random(strconv.Itoa(n), 256))
+	}
+	var err error
+	var d2 *[]Distance
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		d2, err = store.KNN(Random("testV", 256), 5)
+	}
+	e = err
+	d = d2
+	os.Remove("benchmark_index.bin")
+	os.Remove("benchmark_vectors.bin")
+	os.Remove("benchmark_search.bin")
 }
